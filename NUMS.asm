f942b6604dc5885ba7ffa39cba0ba6ee2e64466f
Adriane James Jabagat Puzon <adrianejames27@gmail.com>
2023-12-04 19:23:31 +0800

    add docs

--- NUMS.asm
+++ NUMS.asm
@@ -1,19 +1,27 @@
+; This assembly code prompts the user to enter a number and displays the corresponding word representation of the number.
+; It uses the DOS interrupt 21h to read input from the user and display output on the screen.
+; The program checks the input character and jumps to the corresponding label to display the word representation of the number.
+; If the input is not a valid digit, it displays an error message.
+; After displaying the word representation, the program prompts the user if they want to enter another number.
+; If the user enters 'y' or 'Y', it goes back to the prompt. If the user enters 'n' or 'N', it exits the program.
+
 .model small
 .stack 100h
 .data
-  msg1 db 10, 13, 10, 13, "Enter a number: $"
-  msg2 db 10, 13, "Invalid Input $"
-  msg3 db 10, 13, 10, 13, "Another number (y/n)? $"
-  m0 db 10, 13, "zero $"
-  m1 db 10, 13, "one $"
-  m2 db 10, 13, "two $"
-  m3 db 10, 13, "three $"
-  m4 db 10, 13, "four $"
-  m5 db 10, 13, "five $"
-  m6 db 10, 13, "six $"
-  m7 db 10, 13, "seven $"
-  m8 db 10, 13, "eight $"
-  m9 db 10, 13, "nine $"
+  ; Messages displayed to the user
+  msg1 db 10, 13, 10, 13, "Enter a number: $" ; Prompt to enter a number
+  msg2 db 10, 13, "Invalid Input $" ; Error message for invalid input
+  msg3 db 10, 13, 10, 13, "Another number (y/n)? $" ; Prompt to enter another number
+  m0 db 10, 13, "zero $" ; Word representation of 0
+  m1 db 10, 13, "one $" ; Word representation of 1
+  m2 db 10, 13, "two $" ; Word representation of 2
+  m3 db 10, 13, "three $" ; Word representation of 3
+  m4 db 10, 13, "four $" ; Word representation of 4
+  m5 db 10, 13, "five $" ; Word representation of 5
+  m6 db 10, 13, "six $" ; Word representation of 6
+  m7 db 10, 13, "seven $" ; Word representation of 7
+  m8 db 10, 13, "eight $" ; Word representation of 8
+  m9 db 10, 13, "nine $" ; Word representation of 9
 
 .code
   ; set up data segment
